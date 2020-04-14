065ec808d02597142550732a6d00241802acf2b7
Unnamed123123123121231231212312312312 <48511372+Unnamed123123123121231231212312312312@users.noreply.github.com>
2020-04-14 21:52:07 +0800

    205 CDE project

---
+++ 205CDE Project/forms.py
@@ -0,0 +1,28 @@
+from wtforms import Form,TextField, TextAreaField,StringField, PasswordField, validators, IntegerField
+
+
+
+class RegisterForm(Form):
+	password=PasswordField('password',[validators.DataRequired(),validators.length(min=1,max=50),validators.EqualTo('confirm', message ='Password Not Match')])
+	userName=StringField('userName',[validators.length(min=1,max=50)])
+	email=StringField('email',[validators.length(min=6,max=50),validators.Email("please enter a correct email address" )])
+	address=StringField("address")
+	confirm=PasswordField("Confirmed Password")
+	
+
+class orderForm(Form):
+	dishName=StringField('dishName',[validators.DataRequired()])
+	process_methods=TextAreaField('process_methods',[validators.DataRequired(),validators.length(min=1)])
+	number=StringField('number',[validators.DataRequired(),validators.length(min=1)])
+
+class userNameForm(Form):
+	userName=StringField('userName',[validators.DataRequired()])
+
+class emailForm(Form):
+	email=StringField('email',[validators.Email('please enter a correct email address')])
+
+class passwordForm(Form):	
+	password=StringField('password',[validators.DataRequired()])
+
+class addressForm(Form):	
+	address=StringField('address',[validators.DataRequired()])
